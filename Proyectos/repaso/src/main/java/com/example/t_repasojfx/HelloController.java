e3a5582ee9a455a7dab1159741cc696f0ed4a77d
javierOrt <javito19991@gmail.com>
2021-10-26 17:29:36 +0200

    Primer dia de repaso
    Bastante easy bb

---
+++ Proyectos/repaso/src/main/java/com/example/t_repasojfx/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.t_repasojfx;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    @FXML Button boton1,boton2,boton3;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        acciones();
+    }
+
+    private void acciones() {
+        boton1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado boton1");
+            }
+        });
+        boton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado boton2");
+            }
+        });
+        boton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Pulsado boton2");
+            }
+        });
+    }
+}
