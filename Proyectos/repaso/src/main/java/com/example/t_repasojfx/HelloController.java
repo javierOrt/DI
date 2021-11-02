e6baaafd7deaea42e45f4b72e04810db0d6c06fc
javierOrt <javito19991@gmail.com>
2021-11-02 17:34:47 +0100

    evento key y consume

--- Proyectos/repaso/src/main/java/com/example/t_repasojfx/HelloController.java
+++ Proyectos/repaso/src/main/java/com/example/t_repasojfx/HelloController.java
@@ -6,7 +6,11 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -15,13 +19,26 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable , EventHandler<Event> {
 
     @FXML Button boton1,boton2,boton3;
+    @FXML TextField textoCentro;
 
     DropShadow dropShadow;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instancias();
+        personalizarBotones();
         acciones();
+    }
+
+    private void personalizarBotones() {
+        boton3.setBackground(null);
+        boton3.setBorder(null);
+        boton3.setText("");
+        boton2.setBackground(null);
+        boton2.setBorder(null);
+        boton2.setText("");
+        boton3.setGraphic(new ImageView(new Image(HelloController.class.getResourceAsStream("play.png"))));
+        boton2.setGraphic(new ImageView(new Image(HelloController.class.getResourceAsStream("play2.png"))));
     }
 
     private void instancias() {
@@ -41,6 +58,8 @@ public class HelloController implements Initializable , EventHandler<Event> {
         boton3.addEventHandler(MouseEvent.MOUSE_ENTERED, this);
         boton3.addEventHandler(MouseEvent.MOUSE_EXITED, this);
 
+        textoCentro.addEventHandler(KeyEvent.KEY_RELEASED,this);
+
         /*
         boton1.setOnAction(new ManejoPulsaciones());
         boton2.setOnAction(new ManejoPulsaciones());
@@ -58,25 +77,36 @@ public class HelloController implements Initializable , EventHandler<Event> {
     @Override
     public void handle(Event event) {
 
-        Button btnaux = (Button) event.getSource();
+        Button btnaux = null;
+
+        if (event.getSource() instanceof Button) {
+            btnaux = (Button) event.getSource();
+        }
 
         if (event.getEventType() == ActionEvent.ACTION){
 
             if (event.getSource() == boton1) {
-                System.out.println("Pulsado boton 1");
+                String textoCapturado = textoCentro.getText();
+                System.out.println(textoCapturado);
             } else {
                 System.out.println("Pulsado boton 2 o 3");
             }
 
-        } else if (event.getEventType() == MouseEvent.MOUSE_ENTERED){
-
+        } else if (event.getEventType() == MouseEvent.MOUSE_ENTERED)
             btnaux.setEffect(dropShadow);
-
-        } else if (event.getEventType() == MouseEvent.MOUSE_EXITED){
-
+            else if (event.getEventType() == MouseEvent.MOUSE_EXITED)
             btnaux.setEffect(null);
+
+        if (event.getSource() == boton2){
+            if (event.getEventType() == MouseEvent.MOUSE_ENTERED)
+            boton2.setGraphic(new ImageView(new Image(HelloController.class.getResourceAsStream("play2.png"))));
+            else if (event.getEventType() == MouseEvent.MOUSE_EXITED)
+            boton2.setGraphic(new ImageView(new Image(HelloController.class.getResourceAsStream("play3.png"))));
         }
 
+        if (event.getEventType() == KeyEvent.KEY_RELEASED)
+            if (textoCentro.getText().length()>9)
+                event.consume();
     }
 
     /*
