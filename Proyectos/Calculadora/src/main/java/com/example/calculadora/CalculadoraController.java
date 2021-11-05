8cbed4f6ce49a430458963c58e15d9cbd8f7f09f
javierOrt <javito19991@gmail.com>
2021-11-05 18:27:09 +0100

    Avance calculadora

--- Proyectos/Calculadora/src/main/java/com/example/calculadora/CalculadoraController.java
+++ Proyectos/Calculadora/src/main/java/com/example/calculadora/CalculadoraController.java
@@ -1,14 +1,63 @@
 package com.example.calculadora;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-public class CalculadoraController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ResourceBundle;
+
+public class CalculadoraController implements Initializable, EventHandler<ActionEvent> {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private GridPane gridBtn;
+
+    @FXML
+    private TextField campoTexto;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        acciones();
+    }
+
+    private void acciones() {
+    Collection<Node> gridbtnList = gridBtn.getChildren();
+        for (Node i:
+             gridbtnList) {
+            ((Button) i).setOnAction(this);
+        }
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+        Button btnAux = (Button) actionEvent.getSource();
+        String numeroUno;
+        String numeroDos;
+
+        if (!(btnAux.getText().equals("=") ||
+            btnAux.getText().equals("+") ||
+            btnAux.getText().equals("-") ||
+            btnAux.getText().equals("/") ||
+            btnAux.getText().equals("X") ||
+            btnAux.getText().equals("Borrar")||
+            btnAux.getText().equals("Salir") ||
+            btnAux.getText().equals("%") ||
+            btnAux.getText().equals("AC"))){
+
+            campoTexto.appendText(btnAux.getText());
+            numeroUno = campoTexto.getText();
+            }
+
+            if (btnAux.getText().equals("+")){
+            campoTexto.setText(btnAux.getText());
+        }
     }
 }
