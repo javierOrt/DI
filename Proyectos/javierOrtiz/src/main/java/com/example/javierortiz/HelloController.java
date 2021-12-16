5ebe48187632d5000e2b86f73360dcb3bd7998f8
javierOrt <javito19991@gmail.com>
2021-12-16 11:16:13 +0100

    nomeacuerdo

--- Proyectos/javierOrtiz/src/main/java/com/example/javierortiz/HelloController.java
+++ Proyectos/javierOrtiz/src/main/java/com/example/javierortiz/HelloController.java
@@ -45,15 +45,14 @@ public class HelloController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         instancias();
+        initGUI();
         asociarElementos();
-        imagen();
         lecturaJSON();
-        new Thread(tareaJson).start();
         acciones();
 
     }
 
-    private void imagen() {
+    private void initGUI() {
         imagenJson.setImage(new Image("https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png"));
     }
 
@@ -147,7 +146,7 @@ public class HelloController implements Initializable {
                 InputStream inputStream = new URL(url).openStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String linea = null;
-                StringBuffer stringBuffer = new StringBuffer();
+                StringBuilder stringBuffer = new StringBuilder();
 
                 while ( (linea=bufferedReader.readLine())!=null ){
                     stringBuffer.append(linea);
@@ -161,5 +160,7 @@ public class HelloController implements Initializable {
                 return null;
             }
         };
+
+        new Thread(tareaJson).start();
     }
 }
