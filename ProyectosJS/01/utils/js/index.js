d250cd02c7bbd839e50ae3b2025f4b4783a84809
javierOrt <javito19991@gmail.com>
2022-01-11 16:47:49 +0100

    js momento

---
+++ ProyectosJS/01/utils/js/index.js
@@ -0,0 +1,69 @@
+console.log("Ejecucion de codigo js desde archivo");
+
+let numero = 5;
+let numeroDecimal = 5.7;
+const IVA = 18;
+//var constanteClase = 6;
+
+//tipos de variable
+let numero = 5;
+let numeroDecimal = 5.7;
+let acierto = true;
+let palabra = "palabra";
+let letra = 'c';
+let nulo = null;
+
+let sinDefinir = undefined
+
+
+console.log(typeof numero); 
+console.log(typeof numeroDecimal); 
+console.log(typeof acierto); 
+console.log(typeof palabra); 
+console.log(typeof letra); 
+console.log(typeof nulo); 
+console.log(typeof sinDefinir); 
+
+//console.log("la variable "+numero+" es de tipo "+typeof numero);
+console.log(`la variable ${numero} es de tipo ${typeof numero}`);
+console.log(`la variable ${numeroDecimal} es de tipo ${typeof numeroDecimal}`);
+console.log(`la variable ${acierto} es de tipo ${typeof acierto}`);
+console.log(`la variable ${palabra} es de tipo ${typeof palabra}`);
+console.log(`la variable ${letra} es de tipo ${typeof letra}`);
+console.log(`la variable ${nulo} es de tipo ${typeof nulo}`);
+console.log(`la variable ${sinDefinir} es de tipo ${typeof sinDefinir}`);
+
+let numeroUno = 5;
+let numeroDos = 10;
+
+//let suma += numeroUno
+
+/*console.log(isNaN(palabra));
+
+//alert("Esto es un ejemplo codigo alert")
+let contestacion = confirm()("Estas seguro de la vida")
+if(contestacion){
+    console.log("opcion aceptada")
+}
+else{
+    console.log("opcion no aceptada");
+}
+
+let nombreprompt = prompt("Introduce tu nombre","Nombre");
+*/
+
+/*
+
+        Por favor introduce el nombre
+
+        Bienvenido "" a la app web de operaciones, que operacion deseas hacer
+
+        suma, resta, multiplicacion, division
+
+        perfecto, vamos a sumar, dime el primer operando
+
+        dime segundo operando
+
+        la suma de 4 y 6 es 10
+
+*/
